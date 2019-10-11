de68799d4e4075e337bd62dde1ec5a8b99479bf7
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2019-10-11 21:02:17 +0200

    Switching back to struct

--- Atom Attack/SoundPlayer.swift
+++ Atom Attack/SoundPlayer.swift
@@ -12,23 +12,21 @@ public enum SoundEffects: String {
     case gameOver = "Explosion.mp3"
 }
 
-public class SoundPlayer {
-    class func play(soundEffect: SoundEffects) {
+struct SoundPlayer {
+    static func play(soundEffect: SoundEffects) {
         guard let resourceURL = Bundle.main.url(forResource: soundEffect.rawValue, withExtension: nil) else {
             fatalError("Unable to open sound file")
         }
         
+        //initialize the player and play the sound
+        guard let audioPlayer = try? AVAudioPlayer(contentsOf: resourceURL) else { return } //do not fatalError in order not to interrupt the game if it's only the player who's failing
+        
+        audioPlayer.numberOfLoops = 1
+        audioPlayer.volume = 1
+        
         DispatchQueue.main.async {
-            //initialize the player and play the sound
-            guard let audioPlayer = try? AVAudioPlayer(contentsOf: resourceURL) else { return } //do not fatalError in order not to interrupt the game if it's only the player who's failing
-            
-            audioPlayer.numberOfLoops = 1
-            audioPlayer.volume = 1
-            
-            DispatchQueue.main.async {
-                audioPlayer.prepareToPlay()
-                audioPlayer.play()
-            }
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
         }
     }
 }
